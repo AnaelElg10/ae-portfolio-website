b4dfc2d6e05473d8ed4d8db2724948d8eff18e18
= <=>
2025-08-08 22:59:12 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ae-portfolio-website/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
